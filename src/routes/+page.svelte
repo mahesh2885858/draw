2494fd2732fd7dfa7891753bed9e7d2cf347e16c
mahesh2885858 <mahesh2885858@gmail.com>
2025-04-05 19:38:55 +0530

    draw rectangles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	let canvas: HTMLCanvasElement;
 	let drawing = false;
 	let x = 0;
 	let y = 0;
+	let lastRect = {};
+	let imageData: ImageData;
+	let ctx: CanvasRenderingContext2D;
+
 	const clearCanvas = () => {
-		let ctx = canvas.getContext('2d');
-		console.log({
-			canvasContext: ctx
-		});
 		if (ctx) {
 			ctx.reset();
 		}
@@ -18,29 +20,31 @@
 		x = e.offsetX;
 		y = e.offsetY;
 		drawing = true;
+		if (ctx) {
+			imageData = ctx.getImageData(0, 0, 300, 150);
+		}
 	};
 
 	const onMouseUp = (e: MouseEvent) => {
 		if (drawing) {
-			let ctx = canvas.getContext('2d');
 			if (!ctx) return;
-			drawLine(ctx, x, y, e.offsetX, e.offsetY);
+			drawRect(ctx, x, y, e.offsetX, e.offsetY);
 			x = 0;
 			y = 0;
 			drawing = false;
 		}
-		x = e.offsetX;
-		y = e.offsetY;
+		// x = e.offsetX;
+		// y = e.offsetY;
 		drawing = false;
 	};
 
 	const onMouseMove = (e: MouseEvent) => {
 		if (!drawing) return;
-		let ctx = canvas.getContext('2d');
 		if (!ctx) return;
-		drawLine(ctx, x, y, e.offsetX, e.offsetY);
-		x = e.offsetX;
-		y = e.offsetY;
+		ctx.putImageData(imageData, 0, 0);
+		drawRect(ctx, x, y, e.offsetX, e.offsetY);
+		// x = e.offsetX;
+		// y = e.offsetY;
 	};
 
 	const drawLine = (
@@ -59,11 +63,36 @@
 		ctx.stroke();
 		ctx.closePath();
 	};
+
+	const drawRect = (
+		ctx: CanvasRenderingContext2D,
+		x1: number,
+		y1: number,
+		x2: number,
+		y2: number
+	) => {
+		if (!ctx || !x1 || !y1 || !x2 || !y2) return;
+		ctx.beginPath();
+		ctx.strokeStyle = 'red';
+		ctx.lineWidth = 2;
+		ctx.moveTo(x1, y1);
+		ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+		ctx.stroke();
+		ctx.closePath();
+	};
+
+	onMount(() => {
+		const context = canvas.getContext('2d', { willReadFrequently: true });
+		if (context) {
+			ctx = context;
+		}
+	});
 </script>
 
 <h1>Canvas</h1>
 <button class="rounded-md bg-gray-500 p-2" on:click={clearCanvas}>ClearCanvas</button>
 <canvas
+	class={` ${drawing ? 'cursor-crosshair' : 'cursor-default'}`}
 	on:mousemove={onMouseMove}
 	on:mouseup={onMouseUp}
 	on:mousedown={onMouseDown}
